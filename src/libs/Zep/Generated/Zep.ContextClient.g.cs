ab3724991ff7b86f5d10e9a0c0e4db85a4647e25
Konstantin S. <havendv@gmail.com>
2026-08-14 19:48:55 +0400

    feat: Updated OpenAPI spec (#57)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Zep/Generated/Zep.ContextClient.g.cs
+++ src/libs/Zep/Generated/Zep.ContextClient.g.cs
@@ -33,10 +33,17 @@ namespace Zep
 
         /// <inheritdoc/>
         public global::Zep.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Zep.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Zep.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
